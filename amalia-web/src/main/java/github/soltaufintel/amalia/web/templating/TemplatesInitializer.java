f76107dc656753c11100227c39a3c529e3263a59
Marcus Warm <warm@mwvb.de>
2023-10-03 21:28:22 +0200

    gave up buggy formular generator :-(

--- amalia-web/src/main/java/github/soltaufintel/amalia/web/templating/TemplatesInitializer.java
+++ amalia-web/src/main/java/github/soltaufintel/amalia/web/templating/TemplatesInitializer.java
@@ -1,21 +1,17 @@
 package github.soltaufintel.amalia.web.templating;
 
+import org.pmw.tinylog.Logger;
+
 import com.github.template72.compiler.CompiledTemplates;
 import com.github.template72.compiler.TemplateCompiler;
-import com.github.template72.compiler.TemplateCompilerContext;
-import com.github.template72.compiler.preprocessor.CommentPreprocessor;
-import com.github.template72.compiler.preprocessor.IncludePreprocessor;
-import com.github.template72.compiler.preprocessor.MasterPreprocessor;
-import com.github.template72.compiler.preprocessor.TemplatePreprocessor;
-import com.github.template72.loader.ResourceTemplateLoader;
-import com.github.template72.syntax.TemplateSyntax;
+import com.github.template72.compiler.TemplateCompilerBuilder;
+import com.github.template72.loader.TemplateFileCache;
 
 import github.soltaufintel.amalia.web.action.Page;
 import github.soltaufintel.amalia.web.builder.Initializer;
 import github.soltaufintel.amalia.web.config.AppConfig;
 
 public class TemplatesInitializer implements Initializer {
-	public static Replacer fp;
 	private final String[] pages;
 	
 	/**
@@ -32,21 +28,10 @@ public class TemplatesInitializer implements Initializer {
 	}
 	
 	public static void _init(AppConfig config, String[] pages) {
-		ResourceTemplateLoader loader = new ResourceTemplateLoader() {
-	        @Override
-            public String charsetName() {
-                return "UTF-8";
-            }
-        };
-		
-		TemplateCompilerContext ctx = new TemplateCompilerContext(TemplateSyntax.DEFAULT, loader);
-		fp = new Replacer(ctx, null);
-		TemplatePreprocessor preprocessor = new CommentPreprocessor(ctx,
-					new MasterPreprocessor(ctx,
-					new IncludePreprocessor(ctx, fp)));
-		ctx.setPreprocessor(preprocessor);
-		TemplateCompiler compiler = new TemplateCompiler(ctx);
-		
-		Page.templates = new CompiledTemplates(compiler, new AmaliaTemplateFileCache(), config.isDevelopment(), pages);
+	    for (String template : pages) {
+	        Logger.debug("template: " + template);
+	    }
+        TemplateCompiler compiler = new TemplateCompilerBuilder().withUTF8Loader().build();
+		Page.templates = new CompiledTemplates(compiler, new TemplateFileCache(), config.isDevelopment(), pages);
 	}
 }
