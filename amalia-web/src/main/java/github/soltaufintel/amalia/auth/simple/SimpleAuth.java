43ea3a005eb177e0b4d40dfa49a3782fba3431be
Marcus Warm <warm@mwvb.de>
2024-02-08 13:09:28 +0100

    not needed code

--- amalia-web/src/main/java/github/soltaufintel/amalia/auth/simple/SimpleAuth.java
+++ amalia-web/src/main/java/github/soltaufintel/amalia/auth/simple/SimpleAuth.java
@@ -2,11 +2,8 @@ package github.soltaufintel.amalia.auth.simple;
 
 import github.soltaufintel.amalia.auth.Auth;
 import github.soltaufintel.amalia.auth.IUserService;
-import github.soltaufintel.amalia.auth.rememberme.IKnownUser;
 import github.soltaufintel.amalia.auth.rememberme.NoOpRememberMe;
-import github.soltaufintel.amalia.auth.webcontext.WebContext;
 import github.soltaufintel.amalia.web.config.AppConfig;
-import spark.Spark;
 
 /**
  * Use in WebAppBuilder with <code>.withAuth(new SimpleAuth())</code>
@@ -22,23 +19,5 @@ public class SimpleAuth extends Auth {
     @Override
     protected IUserService getUserService() {
         return userService;
-    }
-    
-    @Override
-    public void filter(WebContext ctx) {
-        String path = ctx.path();
-        if (isProtected(path) && !ctx.session().isLoggedIn()) {
-            IKnownUser knownUser = getRememberMe().getUserIfKnown(ctx);
-            if (knownUser != null) {
-                ctx.session().setUserId(knownUser.getUserId());
-                ctx.session().setLogin(knownUser.getUser());
-                ctx.session().setLoggedIn(true);
-                return;
-            }
-            if (!"/login".equals(path)) {
-                ctx.session().setGoBackPath(path); // Go back to this page after login
-            }
-            Spark.halt(401, (String) ctx.handle(getRoutes().getLoginPageRouteHandler()));
-        }
     }
 }
