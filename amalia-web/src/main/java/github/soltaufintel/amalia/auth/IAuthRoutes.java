94db423a1b475ff4c24e28a74b8f5d26fcf46daa
Marcus Warm <warm@mwvb.de>
2025-01-20 15:51:34 +0100

    setupAuthFilter

--- amalia-web/src/main/java/github/soltaufintel/amalia/auth/IAuthRoutes.java
+++ amalia-web/src/main/java/github/soltaufintel/amalia/auth/IAuthRoutes.java
@@ -3,6 +3,9 @@ package github.soltaufintel.amalia.auth;
 import github.soltaufintel.amalia.web.route.RouteHandler;
 import github.soltaufintel.amalia.web.route.Routes;
 
+/**
+ * You should call setupAuthFilter() in routes() !
+ */
 public interface IAuthRoutes extends Routes {
 
     RouteHandler getLoginPageRouteHandler();
