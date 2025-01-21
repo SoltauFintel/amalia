63aa0435ec7b6389e159b6425406d0498369ddea
Marcus Warm <warm@mwvb.de>
2025-01-21 15:27:01 +0100

    queryParamToList, contentDisposition, session, contentType...,
    queryParamsValues and maxAge as new Context methods

--- amalia-web/src/main/java/github/soltaufintel/amalia/spark/Context.java
+++ amalia-web/src/main/java/github/soltaufintel/amalia/spark/Context.java
@@ -1,5 +1,9 @@
 package github.soltaufintel.amalia.spark;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import spark.Request;
 import spark.Response;
 
@@ -40,6 +44,27 @@ public class Context {
         return req.queryParams(key);
     }
     
+    /**
+     * Values are separated with ","
+     * @param name name of query parameter
+     * @return values as List
+     */
+    public List<String> queryParamToList(String name) {
+        List<String> ret = new ArrayList<>();
+        String list = req.queryParams(name);
+        if (list != null && !list.isBlank()) {
+            for (String value : list.split(",")) {
+                ret.add(value);
+            }
+        }
+        return ret;
+    }
+
+    public List<String> queryParamsValues(String key) {
+        String[] a = req.queryParamsValues(key);
+        return a == null ? null : Arrays.asList(a);
+    }
+    
     public String formParam(String key) {
         return req.queryParams(key);
     }
@@ -50,5 +75,89 @@ public class Context {
 
     public String body() {
         return req.body();
+    }
+
+    /**
+     * @param name name of session attribute
+     * @return value of session attribute
+     */
+    public String session(String name) {
+        return req.session().attribute(name);
+    }
+    
+    /**
+     * @param name name of session attribute
+     * @param value value of session attribute
+     */
+    public void session(String name, String value) {
+        req.session().attribute(name, value);
+    }
+
+    /**
+     * Sets header Content-Type = text/csv
+     */
+    public void contentTypeCSV() {
+        res.header("Content-Type", "text/csv");
+    }
+
+    /**
+     * Sets header Content-Type = application/json
+     */
+    public void contentTypeJSON() {
+        res.header("Content-Type", "application/json");
+    }
+
+    /**
+     * Sets header Content-Type = application/pdf
+     */
+    public void contentTypePDF() {
+        res.header("Content-Type", "application/pdf");
+    }
+
+    /**
+     * Sets header Content-Type = image/png
+     */
+    public void contentTypePNG() {
+        res.header("Content-Type", "image/png");
+    }
+
+    /**
+     * Sets header Content-Type = application/zip
+     */
+    public void contentTypeZip() {
+        res.header("Content-Type", "application/zip");
+    }
+    
+    public enum ContentDisposition {
+        // lowercase!
+        /** Opens file in browser */
+        inline,
+        /** Shows file in downloads */
+        attachment;
+    }
+    
+    /**
+     * Setes header Content-Disposition
+     * @param contentDisposition inline or attachment
+     * @param filename without path
+     */
+    public void contentDisposition(ContentDisposition contentDisposition, String filename) {
+        res.header("Content-Disposition", contentDisposition.name() + (filename == null || filename.isBlank() ? "" : "; filename=\"" + filename + "\""));
+    }
+    
+    public void attachment(String filename) {
+        contentDisposition(ContentDisposition.attachment, filename);
+    }
+
+    public void inline(String filename) {
+        contentDisposition(ContentDisposition.inline, filename);
+    }
+
+    /**
+     * Sets header Cache-Control = max-age= minutes * 60
+     * @param minutes -
+     */
+    public void maxAge(int minutes) {
+        res.header("Cache-Control", "max-age=" + (minutes * 60));
     }
 }
