45e168015f91c763d382d7109f3aa87a6227afbe
Marcus Warm <warm@mwvb.de>
2025-08-01 15:47:45 +0200

    TODO

--- amalia-web/src/main/java/github/soltaufintel/amalia/web/action/PageInitializer.java
+++ amalia-web/src/main/java/github/soltaufintel/amalia/web/action/PageInitializer.java
@@ -2,7 +2,7 @@ package github.soltaufintel.amalia.web.action;
 
 import github.soltaufintel.amalia.spark.Context;
 
-public class PageInitializer {
+public class PageInitializer { // TODO Eigentlich könnte das doch ein Interface sein.
     
     public void initPage(Context ctx, Page page) {
         page.put("title", "Web app");
