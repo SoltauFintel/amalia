4c6a73bf4935285837a26d30af279c8dc95e511d
Marcus Warm <warm@mwvb.de>
2025-11-13 20:17:12 +0100

    devtools

--- amalia-web/src/main/java/github/soltaufintel/amalia/web/action/GuruError404Page.java
+++ amalia-web/src/main/java/github/soltaufintel/amalia/web/action/GuruError404Page.java
@@ -7,7 +7,7 @@ public class GuruError404Page extends AbstractGuruErrorPage {
     @Override
     protected void execute() {
         String msg = "Page not found. (404)";
-        if (!"/fonts/glyphicons-halflings-regular.woff2".equals(ctx.path())) {
+        if (!"/fonts/glyphicons-halflings-regular.woff2".equals(ctx.path()) && !ctx.path().startsWith("/.")) {
             Logger.error("Error rendering path \"" + ctx.path() + "\": " + msg);
         }
         ctx.status(404);
