3b6f18d3fdcb2f841df41071384b33e13eaa13c0
Marcus Warm <m4swarm@gmail.com>
2026-03-19 19:07:34 +0100

    Context.fullPath

--- amalia-web/src/main/java/github/soltaufintel/amalia/web/action/GuruError404Page.java
+++ amalia-web/src/main/java/github/soltaufintel/amalia/web/action/GuruError404Page.java
@@ -7,8 +7,8 @@ public class GuruError404Page extends AbstractGuruErrorPage {
     @Override
     protected void execute() {
         String msg = "Page not found. (404)";
-        if (!"/fonts/glyphicons-halflings-regular.woff2".equals(ctx.path())) {
-            Logger.error("Error rendering path \"" + ctx.path() + "\": " + msg);
+        if (!"/fonts/glyphicons-halflings-regular.woff2".equals(ctx.path()) && !ctx.path().startsWith("/.")) {
+            Logger.error("Error rendering path \"" + ctx.fullPath() + "\": " + msg);
         }
         ctx.status(404);
         setMsg(msg);
